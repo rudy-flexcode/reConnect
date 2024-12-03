d339101e0e102fc85eb66a7dff36e290f9824d03
Camille L <b.lemortcamille@gmail.com>
2024-12-03 14:43:45 +0100

    Ajout card container sur echanges

--- client/src/pages/echanges.tsx
+++ client/src/pages/echanges.tsx
@@ -1,7 +1,10 @@
+import CardContainer from "../components/CardContainer";
+
 const Echanges = () => {
   return (
     <div>
       <h1>POUETTE ECHANGE</h1>
+      <CardContainer />
     </div>
   );
 };

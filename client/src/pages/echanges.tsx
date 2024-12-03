461e8f2f6b65eedf99093838205eacaf75bba78d
marco rojouan <marcoprojouan@outlook.fr>
2024-12-03 16:10:56 +0100

    feat: wanted section

--- client/src/pages/echanges.tsx
+++ client/src/pages/echanges.tsx
@@ -1,7 +1,40 @@
+import { useEffect, useState } from "react";
+import CardContainer from "../components/CardContainer";
+import WantedArticles from "../components/WantedArticles";
+import type { ArticleType } from "../lib/definition";
+
 const Echanges = () => {
+  const [articles, setArticles] = useState<ArticleType[]>([]);
+  const [isWanted, setIsWanted] = useState<ArticleType[]>([]);
+
+  useEffect(() => {
+    fetch("https://api-re-connect-t3ln.vercel.app/api/resources")
+      .then((response) => response.json())
+      .then((data) => setArticles(data));
+  }, []);
+
+  const changeWantedValue = (id: number) => {
+    setArticles((currentArticles) =>
+      currentArticles.map((wantedArticle) =>
+        wantedArticle.id === id
+          ? { ...wantedArticle, disponibilite: !wantedArticle.disponibilite }
+          : wantedArticle,
+      ),
+    );
+  };
+
+  useEffect(() => {
+    const wanted = articles.filter((article) => !article.disponibilite);
+    setIsWanted(wanted);
+  }, [articles]);
+
   return (
     <div>
-      <h1>POUETTE ECHANGE</h1>
+      <CardContainer
+        articles={articles}
+        changeWantedValue={changeWantedValue}
+      />
+      <WantedArticles isWanted={isWanted} />
     </div>
   );
 };

03a6e6d76a2a96b898d84a8de59a8bfd83906393
Malaurynn <malauryn@icloud.com>
2024-12-03 17:22:37 +0100

    ajout des link

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,4 +1,5 @@
 import Logo from "../assets/logo.png";
+import { Link } from 'react-router-dom';
 import "../styles/Header.css";
 
 const Header = () => {
@@ -9,8 +10,8 @@ const Header = () => {
         <h1>Ensemble, partageons avec bienveillance.</h1>
       </div>
       <div className="nav-links">
-        <h2>Accueil</h2>
-        <h2>Biens et services</h2>
+      <Link to="/">Home</Link>
+      <Link to="/echanges">Bien et Services</Link>
       </div>
     </header>
   );
